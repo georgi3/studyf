43dbe57304f60cf22e160444755fb966b5e3aee2
George Marchenko <george.marchenko44@gmail.com>
2022-11-22 10:00:26 -0500

    added stylesheet link for font-awesome

--- partials/base.php
+++ partials/base.php
@@ -8,6 +8,7 @@ require_once 'includes/ti/ti.php';
         <title>StudyForever</title>
         <link rel='stylesheet' type='text/css' href='assets/style/style.css'/>
         <link rel='stylesheet' type='text/css' href='assets/style/base.css'/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <?php if (!isset($_SESSION['userLoggedIn'])) : ?>
@@ -23,7 +24,7 @@ require_once 'includes/ti/ti.php';
         <div id="navbar" class="navbar">
             <a href="teacherDashboard.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/teacherDashboard.php' ? 'active' : '')?>'>Dashboard</a>
             <a href="about.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/about.php' ? 'active' : '')?>'>Grades.easy</a>
-            <a href="account.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/account.php' ? 'active' : '')?>'>
+            <a href="profilePage.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/profilePage.php' ? 'active' : '')?>'>
                 <?php echo $_SESSION['userLoggedIn'] ?></a>
             <a href="logout.php" class='auth'>Log Out</a>
         </div>
@@ -34,7 +35,7 @@ require_once 'includes/ti/ti.php';
             <a href="studentDashboard.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/studentDashboard.php' ? 'active' : '')?>'>Dashboard</a>
             <a href="explorePage.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/explorePage.php' ? 'active' : '')?>'>Explore</a>
             <a href="about.php" class='<?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/about.php' ? 'active' : '')?>'>Grades.easy</a>
-            <a href="account.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/account.php' ? 'active' : '')?>'>
+            <a href="profilePage.php" class='auth <?php echo ($_SERVER['PHP_SELF'] == '/soen_proj/profilePage.php' ? 'active' : '')?>'>
                 <?php echo $_SESSION['userLoggedIn'] ?></a>
             <a href="logout.php" class='auth'>Log Out</a>
         </div>
@@ -53,7 +54,31 @@ require_once 'includes/ti/ti.php';
 
 
         <!-- FOOTER -->
-        <div class="footer">
+        <div class="footer-container">
+            <div class="footer">
+                <div class="footer-heading">
+                    <h2 style="color: gold">social media</h2>
+
+                    <a href="">Instagram</a>
+                    <a href="">FaceBook</a>
+                    <a href="">Blog</a>
+
+                </div>
+                <div class="footer-heading">
+                    <h2 style="color: gold">contact us</h2>
+                    <a href="">Email</a>
+                    <a href="">Telephone</a>
+                    <a href="">Address</a>
+
+                </div>
+                <div class="footer-heading">
+                    <h2 style="color: gold">useful links</h2>
+
+                    <a href="/soen_proj/about.php">Home</a>
+                    <a href="/soen_proj/register.php">Register</a>
+                    <a href="/soen_proj/login.php">Login</a>
+                </div>
+            </div>
         </div>
 <!--         FOOTER END-->
         <script src="assets/js/main.js"></script>
