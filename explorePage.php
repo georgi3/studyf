ed3eedf3f08e6faf79697e23dbe3f4bab32eb130
George Marchenko <george.marchenko44@gmail.com>
2022-12-07 20:46:48 -0500

    added levenshtein algo for searching; autocompletion form

--- explorePage.php
+++ explorePage.php
@@ -1,18 +1,103 @@
 <?php
 include 'partials/base.php';
 include_once 'partials/authCheck.php';
-
+require_once 'includes/classes/ExistingCourses.php';
+require_once 'includes/classes/EnrolledCourses.php';
+require_once 'includes/classes/FormSanitizer.php';
 protectStudentProperty();
+
+$coursesConnection = new ExistingCourses($connection);
+$courseEnrolment = new EnrolledCourses($connection);
+$autocompleteOptions = $coursesConnection->getCoursesTags();
+
+if (isset($_POST['searchCourses'])){
+    // Sanitize
+    $queryString = FormSanitizer::sanitizeString($_POST['courseSearchBar']);
+    $searchedCourses = $coursesConnection->findSearchedCourses($queryString);
+    $usersEnrolledCourses = $courseEnrolment->enrolledCourses($_SESSION['userLoggedIn']);
+}
+if (isset($_POST['EnrollInCourse'])){
+    $courseCode = $_POST['courseID'];
+    $courseEnrolment->enrollInCourse($_SESSION['userLoggedIn'],$courseCode);
+    header("Location: /soen_proj/studentDashboard.php");
+}
+//$autocompleteOptions = array_values($autocompleteOptions);
 startblock('main');
+?>
+<?php
+foreach ($autocompleteOptions as $option){
+    echo "<option class='autocompleteOptions' style='visibility: collapse; display: none;'>{$option}</option>";
+}
 ?>
 <div class="explorePage">
     <div class="searchBarContainer">
         <div class="searchBar">
             <h3>Explore all possible courses here</h3>
-            <form action="" method="POST">
-                <input type="text" placeholder="Enter topic of interest or Course Code">
+            <form action="" method="POST" autocomplete="off">
+                <div class="searchBarAutoComplete">
+                    <input type="text" id="courseSearchBar" name="courseSearchBar" placeholder="Enter topic of interest or Course Code">
+                    <button type="submit" name="searchCourses"><i class="fa fa-search"></i></button>
+                </div>
             </form>
         </div>
+    </div>
+    <?php
+    if(isset($_POST['searchCourses'])){
+        echo "<div class='searchResults'>";
+    }
+    ?>
+    <?php
+    if(isset($_POST['searchCourses'])){
+        echo "<h1>Search Results for {$queryString}</h1>";
+        echo "<div class='suggestedCourses'>";
+//        echo gettype($usersEnrolledCourses), "{$usersEnrolledCourses}";
+//        $i = 0;
+        foreach ($searchedCourses as $course){
+            $filePath = "/Applications/XAMPP/xamppfiles/htdocs/soen_proj/data/{$course[4]}.json";
+            $openedFile = file_get_contents($filePath);
+            $courseJSON = json_decode($openedFile, true);
+            echo "
+                              <div class='suggestedCourse'>
+                                  <div class='courseTitle'>
+                                      <h3>{$course[2]}</h3>
+                                  </div>
+                                  <div class='courseImage'>
+                                      <img src='assets/imgs/jsIMG.png' alt='Course Img'>
+                                  </div>
+                                  <div class='courseTags'>
+                                  ";
+            foreach (explode(',', $course[3]) as $courseTag){
+                echo "
+                                      <p>{$courseTag}</p>
+                                  ";
+            }
+            echo "
+                                  </div>
+                                  <div class='courseDesc'>
+                                      <p>{$courseJSON['courseDesc']}</p>
+                                  </div>  
+                              ";
+            if (in_array($course[0], $usersEnrolledCourses)){
+                echo "
+                              <div class='alreadyEnrolled'>Already Enrolled</div>   
+                ";
+            } else{
+                echo "
+                              <form action='' method='POST'>
+                                  <input type='hidden' name='courseID' value='{$course[4]}'>
+                                  <input type='submit' class='enrollInCourse' name='EnrollInCourse' value='Enroll' />
+                               </form>
+                ";
+            }
+            echo "</div>";
+        }
+    }
+    ?>
+    <?php
+    if(isset($_POST['searchCourses'])){
+        echo "</div>";
+    }
+    ?>
     </div>
     <div class="suggestedCoursesContainer">
         <h1>Explore Courses Here</h1>
