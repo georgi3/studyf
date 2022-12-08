8d068bece70c7794abb201054ca0e75313d2afdf
George Marchenko <george.marchenko44@gmail.com>
2022-12-07 20:58:32 -0500

    init

---
+++ teacherGradesPage.php
@@ -0,0 +1,194 @@
+<?php
+include 'partials/base.php';
+include_once 'partials/authCheck.php';
+protectTeacherProperty();
+
+startblock('main');
+?>
+<div class="scheduleDiv">
+  <div id="scheduleContainers">
+    <div id="fallSchedule">
+      <h3>Fall 2022</h3>
+      <ul>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div id="winterSchedule">
+      <h3>Winter 2023</h3>
+      <ul>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h4>COMP 353 - DATABASES</h4>
+          <ul>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td></tr>
+              </table>
+            </li>
+            <li>
+              <table>
+                <tr><th>STATUS</th> <th>UNITS</th> <th>GRADING</th> <th>GRADE</th> <th>STATUS</th> <th>UNITS</th> <th>GRADING</th></tr>
+                <tr><td>STATUS</td> <td>UNITS</td> <td>GRADING</td> <td>GRADE</td> <td>STATUS</td> <td>UNITS</td> <td>GRADING</td></tr>
+              </table>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+<?php
+endblock();
+?>
